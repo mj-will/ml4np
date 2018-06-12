f00009c15970911ce32087202dd94576a230812d
Michael Williams <mikew@np-phi.physics.gla.ac.uk>
2018-06-12 15:13:33 +0100

    Changes for new dataset

--- pyroot/ClassificationKeras.py
+++ pyroot/ClassificationKeras.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
-from ROOT import TMVA, TFile, TTree, TCut
-from subprocess import call
-from os.path import isfile, isdir
+import sys
 import os
+#sys.path = [''] + sys.path
+#print(sys.path)
+#print(sys.version)
+#print(sys.version_info)
+#print(sys.executable)
+
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
+from keras.layers import BatchNormalization, AlphaDropout
 from keras.regularizers import l2
 from keras import initializers
 from keras.optimizers import SGD, Nadam
+
+import ROOT
+from ROOT import TMVA, TFile, TTree, TCut
+from subprocess import call
+from os.path import isfile, isdir
 
 # Setup TMVA
 TMVA.Tools.Instance()
@@ -27,8 +37,8 @@ output = TFile.Open('TMVA.root', 'UPDATE')
 factory = TMVA.Factory('TMVAClassification', output,
         '!V:!Silent:Color:DrawProgressBar:Transformations=D,G:AnalysisType=Classification')
 
-fnameSignal = '../data/rho10_tm1_sm1_stat/Signal.root'
-fnameBackground = '../data/rho10_tm1_sm1_stat/Background.root'
+fnameSignal = '../data/tmva3/Signal2Pi.root'
+fnameBackground = '../data/tmva3/BG2Pi.root'
 
 # Load data
 if not isfile(fnameSignal):
@@ -38,19 +48,41 @@ if not isfile(fnameBackground):
 
 dataSignal = TFile.Open(fnameSignal)
 dataBackground = TFile.Open(fnameBackground)
-signal = dataSignal.Get('HSParticles')
-background = dataBackground.Get('HSParticles')
+
+
+signal0 = dataSignal.Get('HSParticles')
+background0 = dataBackground.Get('HSParticles')
+
+
+def skim(tree):
+    """
+    Skim tree for nulls and infs
+    """
+    ROOT.gROOT.cd()
+    t = tree.CopyTree('Detector==0&&PipTime!=0&&PimTime!=0&&PTime!=0')
+    return t
+
+# remove nulls
+signal = skim(signal0)
+background = skim(background0)
+
+ROOT.gROOT.cd()
 
 # make data loader
 dataloader = TMVA.DataLoader('dataset')
 
-# list of variables to include
-include = ['ElTime', 'ElP', 'ElTh', 'ElPhi', 'PTime', 'PP', 'PTh', 'PPhi', 'PimTime', 'PimP', 'PimTh', 'PimPhi', 'PipTime', 'PipP', 'PipTh', 'PipPhi']
+# list of variables to exclude
+exclude = ['MissMass2', 'MissMass', 'NPerm' 'ElDet', 'Correct', 'UID', 'Topo', 'ElDeltaE', 'PDeltaE', 'PipDeltaE', 'PimDeltaE', 'ElTrChi2', 'PTrChi2', 'PipTrChi2', 'PimTrChi2', 'ElDet', 'Detector', 'ElEdep', 'PEdep', 'PipEdep', 'PimEdep', 'ElPreE', 'PPreE', 'PipPreE', 'PimPreE', 'ElVz', 'PVz', 'PipVz', 'PimVz']
+
+# list of variables to use
+branches = signal.GetListOfBranches()
+include = [b.GetName() for b in branches if b.GetName() not in exclude]
+
+# number of variables for output layer
 Nvar = len(include)
 # loop over branches
-for branch in signal.GetListOfBranches():
-    if branch.GetName() in include:
-        dataloader.AddVariable(branch.GetName())
+for b in include:
+    dataloader.AddVariable(b)
 
 dataloader.AddSignalTree(signal, 1.0)
 dataloader.AddBackgroundTree(background, 1.0)
@@ -60,13 +92,30 @@ dataloader.PrepareTrainingAndTestTree(TCut(''),
 # Generate model
 
 # Define model
+"""
 model = Sequential()
-model.add(Dense(64, activation='relu', kernel_regularizer=l2(1e-5), input_dim=Nvar))
-model.add(Dense(32, activation='relu', kernel_regularizer=l2(1e-5), input_dim=Nvar))
-#model.add(Dropout(0.))
-model.add(Dense(16, activation='relu', kernel_regularizer=l2(1e-5)))
-model.add(Dropout(0.1))
-#model.add(Dense(32,  activation='relu', kernel_regularizer=l2(1e-5)))
+model.add(Dense(1024, input_dim=Nvar))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+model.add(Dense(512, ))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+model.add(Dense(2, activation='softmax'))
+"""
+# SNN model
+model = Sequential()
+model.add(Dense(1024,input_dim=Nvar,
+    kernel_initializer=initializers.lecun_uniform(),
+    bias_initializer=initializers.lecun_uniform()))
+model.add(Activation('selu'))
+model.add(AlphaDropout(0.5))
+model.add(Dense(512,
+    kernel_initializer=initializers.lecun_uniform(),
+    bias_initializer=initializers.lecun_uniform()))
+model.add(Activation('selu'))
+model.add(AlphaDropout(0.5))
 model.add(Dense(2, activation='softmax'))
 
 # Set loss and optimizer
@@ -74,11 +123,12 @@ model.compile(loss='categorical_crossentropy', optimizer=Nadam(), metrics=['accu
 
 # Store model to file
 model.save('{0}/model{1}.h5'.format(model_path, i))
+model.save('./model.h5')
 model.summary()
 
 # Book methods
 
-#factory.BookMethod(dataloader, TMVA.Types.kBDT, 'BDT',"!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20")
+factory.BookMethod(dataloader, TMVA.Types.kBDT, 'BDT',"!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20")
 
 #factory.BookMethod(dataloader, TMVA.Types.kBDT, 'BDT+',"!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20")
 
---
+++ pyroot/plots.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+from ROOT import TMVA
+
+# Setup TMVA
+TMVA.Tools.Instance()
+TMVA.PyMethodBase.PyInitialize()
+
+
+
